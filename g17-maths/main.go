12abba8289e7bb621c265f1d9d87f05eb2948fe9
neimv <neimv.zatara@gmail.com>
2021-11-29 08:31:57 -0600

    chapter 17

---
+++ g17-maths/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
